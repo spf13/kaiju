f5bed79058daf5cab1a95b1fed4507b22aea2334
Jeremy Mikola <jmikola@gmail.com>
2014-02-05 15:53:46 -0500

    Comment and User models

---
+++ models/comment.go
@@ -0,0 +1,19 @@
+package models
+
+import {
+    "labix.org/v2/mgo/bson"
+}
+
+type CommentUser struct {
+    FullName string
+    UserId bson.ObjectId
+}
+
+type Comment struct {
+    Id bson.ObjectId "_id"
+    Page string
+    User CommentUser
+    Body string
+    Parent bson.ObjectId
+    Children []bson.ObjectId
+}
---
+++ models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import {
+    "labix.org/v2/mgo/bson"
+}
+
+type UserService struct {
+    Provider string
+    UserId string
+}
+
+type User struct {
+    Id bson.ObjectId "_id"
+    FullName string
+    Website string
+    Location string
+    Bio string
+    Services []UserService
+}
